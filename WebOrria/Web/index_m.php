7855bfa86452bb99292f814da3365604c9d67acf
Hodetxe <hod_etx_azu@goierrieskola.org>
2025-01-24 10:33:18 +0100

    fitxategiak aldatuta eta zebitzuak zuzendu

--- WebOrria/Web/index_m.php
+++ WebOrria/Web/index_m.php
@@ -1,24 +1,41 @@
+
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
-    <link rel="stylesheet" href="css_m.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <title>Hasiera</title>
+    <link rel="stylesheet" href="css_p.css">
+    <title>FROGA</title>
 </head>
-
-<body>
-    <header>
+<body class="grid-container">
+    <header class="header">
         <?php require_once "header_m.php" ?>
     </header>
-
-    <div class="content">
+    <nav class="nav">
         <?php require_once "menu.php" ?>
-    </div>
-    
-    <footer>
+    </nav>
+    <article class="main">
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sociosqu arcu, vulputate sed litora dictumst commodo in
+                gravida torquent viverra, nec laoreet placerat urna vivamus rutrum interdum augue. Orci curabitur per 
+                facilisis duis posuere fusce leo bibendum dictum, sem maecenas tincidunt varius pharetra lacinia lobortis lectus.
+                Platea facilisis interdum curabitur vulputate felis gravida montes inceptos aliquam euismod phasellus ornare, 
+                malesuada laoreet est ultricies lacinia mus hac et hendrerit venenatis. Hendrerit curabitur sollicitudin placerat 
+                egestas luctus felis, aptent pellentesque feugiat c
+                onsequat tincidunt auctor non, velit accumsan euismod sociis ridiculus.
+                Nascetur quisque quam cras integer mus sagittis tellus venenatis, imperdiet nibh neque tristique potenti vel
+                sapien viverra, class eu justo diam orci porta fringilla. Elementum consequat nisl suscipit sagittis vel cubilia 
+                congue nec integer, natoque bibendum primis fusce augue cras nam facilisi, mi curae eget montes ac habitasse suspendisse turpis. 
+                Imperdiet vestibulum laoreet natoque ultricies maecenas inceptos mollis facilisis leo, a at porttitor himenaeos dapibus fermentum 
+                varius aenean, bibendum justo congue feugiat netus nostra fusce erat. Cubilia vel 
+                class accumsan nec curae himenaeos neque, orci iaculis tincidunt sapien per ultrices eget molestie, velit quisque 
+                euismod hac diam sem.</p>
+        </div>
+           </article>
+    <footer class="footer">
         <?php require_once "footer_m.php" ?>
     </footer>
+    
 </body>
-
 </html>
