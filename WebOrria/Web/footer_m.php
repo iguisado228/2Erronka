ac93b1a66ec92ff49165a33bd1cb4fbc3f150335
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-01-27 10:13:50 +0100

    Footerra eginda

--- WebOrria/Web/footer_m.php
+++ WebOrria/Web/footer_m.php
@@ -1,8 +1,13 @@
 <head>
   <link rel="stylesheet" href="css_m.css">
+  <script src="https://kit.fontawesome.com/83f15f6aab.js" crossorigin="anonymous"></script>
 </head>
-
-
-<div class="footer">
-    <p>fhsdbfjashdfjsdafkjhsdajf</p>
-</div>
+<footer>
+  <div class="footer">
+    <div class="foothelb footleft"><i class="fa-solid fa-location-dot"></i> Elbarrena Auzoa, 30, 20150 Aduna, Gipuzkoa
+    </div>
+    <div class="footmail footleft"><i class="fa-solid fa-envelope"></i> administrazioa@TEK8.com</div>
+    <div class="foottel footleft"><i class="fa-solid fa-phone"></i> +34 612 34 90 73</div>
+  <div class="footrights"><i class="fa-solid fa-copyright"></i> 2025 TEK8 S.A.</div>
+  </div>
+</footer>
